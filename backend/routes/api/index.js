68bd38a6f17a01c618e816ee4d3bb12fa34352e2
Shane Wilkey <mail@southwestmogrown.com>
2023-03-07 00:52:02 -0600

    forgot to save, doh!

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,37 +2,10 @@ const router = require('express').Router();
 const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
 
+// Connect restoreUser middleware to the API router
+  // If current user session is valid, set req.user to the user in the database
+  // If current user session is not valid, set req.user to null
 router.use(restoreUser);
-
-router.get(
-    '/restore-user',
-    (req, res) => {
-        return res.json(req.user);
-    }
-)
-
-router.get(
-  '/require-auth',
-  requireAuth,
-  (req, res) => {
-    return res.json(req.user);
-  }
-);
-
-router.post('/test', (req, res) => {
-    res.json({ requestBody: req.body });
-});
-
-router.get('/set-token-cookie', async (_req, res) => {
-    const user = await User.findOne({
-        where: {
-            userName: 'Demo-lition'
-        }
-    });
-    setTokenCookie(res, user);
-    return res.json({ user: user });
-});
-
 
 
 module.exports = router;
