2058ad5bcace679b6be2fc77efafdc12315b2cfd
Shane Wilkey <mail@southwestmogrown.com>
2023-03-06 23:53:31 -0600

    Added Users table to migration, seeded demo users

---
+++ backend/db/seeders/20230307054015-demo-user.js
@@ -0,0 +1,34 @@
+'use strict';
+const bcrypt = require("bcryptjs");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert("Users", [
+      {
+        email: 'demo@user.io',
+        userName: 'Demo-lition',
+        hashedPassword: bcrypt.hashSync('password'),
+        userTypeId: 1
+      },
+      {
+        email: 'user1@user.io',
+        userName: 'FakeUser1',
+        hashedPassword: bcrypt.hashSync('password2'),
+        userTypeId: 1
+      },
+      {
+        email: 'user2@user.io',
+        userName: 'FakeUser2',
+        hashedPassword: bcrypt.hashSync('password3'),
+        userTypeId: 2
+      }
+    ], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete("Users", {
+      userName: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
+    }, {});
+  }
+};
