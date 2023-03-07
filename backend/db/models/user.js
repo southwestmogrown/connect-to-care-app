10f89602e2ae60d7232fcf620cd35c56b9e63f08
Shane Wilkey <mail@southwestmogrown.com>
2023-03-07 00:12:19 -0600

    added User model authentication and sequelize scopes and methods.

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,4 +1,6 @@
 'use strict';
+const bcrypt = require('bcryptjs');
+
 const {
   Model,
   Validator
@@ -6,11 +8,50 @@ const {
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+    
+    toSafeObject() {
+      const {id, userTypeId, userName, email } = this;
+      return { id, userTypeId, userName, email}
+    }
+
+    validatePassword(password) {
+      return bcrypt.compareSync(password, this.hashedPassword.toString());
+    }
+
+    static getCurrentUserById(id) {
+      return User.scope("currentUser").findByPk(id);
+    }
+
+    static async login({ credential, password }) {
+      const { Op } = require('sequelize');
+      const user = await User.scope('loginUser').findOne({
+        where: {
+          [Op.or]: {
+            userName: credential,
+            email: credential
+          }
+        }
+      });
+
+      if (user && user.validatePassword(password)) {
+        return await User.scope('currentUser').findByPk(user.id);
+      }
+    }
+
+    static async signup({ userTypeId, userName, email, password, }) {
+
+      const hashedPassword = bcrypt.hashSync(password);
+
+      const user = await User.create({
+        userTypeId,
+        userName,
+        email,
+        hashedPassword
+      });
+
+      return await User.scope('currentUser').findByPk(user.id);
+    }
+
     static associate(models) {
       // define association here
     }
@@ -44,9 +85,28 @@ module.exports = (sequelize, DataTypes) => {
         len: [60, 60]
       }
     }
-  }, {
+  }, 
+  {
     sequelize,
     modelName: 'User',
-  });
+  },
+  {
+    sequelize,
+    modelName: "User",
+    defaultScope: {
+      attributes: {
+        exclude: ["hashedPassword", "email", "createdAt", "updatedAt"]
+      }
+    },
+    scopes: {
+      currentUser: {
+        attributes: { exclude: ["hashedPassword"] }
+      },
+      loginUser: {
+        attributes: {}
+      }
+    }
+  }
+  );
   return User;
 };
