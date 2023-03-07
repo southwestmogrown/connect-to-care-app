79731e5e21926f7849a5db7886cb675538a318a8
Shane Wilkey <mail@southwestmogrown.com>
2023-03-07 11:13:38 -0600

    Added signup backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,3 +1,17 @@
 const router = require('express').Router();
 
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
+router.post('/', async (req, res) => {
+    const { email, password, userName, userTypeId } = req.body;
+    const user = await User.signup({ userTypeId, userName, email, password });
+
+    await setTokenCookie(res, user);
+
+    return res.json({
+        user: user
+    });
+});
+
 module.exports = router;
