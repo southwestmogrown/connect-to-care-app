fcac2adb67271438ec269b8e2d3ec31f4b4ad2df
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2023-05-17 20:32:59 -0500

    setting up for render deployment

--- backend-backup/db/models/user.js
+++ backend-backup/db/models/user.js
@@ -1,16 +1,12 @@
 'use strict';
 const bcrypt = require('bcryptjs');
 
-const {
-  Model,
-  Validator
-} = require('sequelize');
+const { Model, Validator } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    
     toSafeObject() {
-      const {id, userTypeId, userName, email, firstName, lastName } = this;
+      const { id, userTypeId, userName, email, firstName, lastName } = this;
       return { id, userTypeId, userName, email, firstName, lastName };
     }
 
@@ -19,30 +15,33 @@ module.exports = (sequelize, DataTypes) => {
     }
 
     static getCurrentUserById(id) {
-      return User.scope("currentUser").findByPk(id);
+      return User.scope('currentUser').findByPk(id);
     }
 
     static async login({ credential, password }) {
-     
       const { Op } = require('sequelize');
       const user = await User.scope('loginUser').findOne({
         where: {
           [Op.or]: {
             userName: credential,
-            email: credential
-          }
-        }
+            email: credential,
+          },
+        },
       });
-
-      console.log(user)
 
       if (user && user.validatePassword(password)) {
         return await User.scope('currentUser').findByPk(user.id);
       }
     }
 
-    static async signup({ userTypeId, userName, email, password, firstName, lastName }) {
-
+    static async signup({
+      userTypeId,
+      userName,
+      email,
+      password,
+      firstName,
+      lastName,
+    }) {
       const hashedPassword = bcrypt.hashSync(password);
 
       const user = await User.create({
@@ -51,7 +50,7 @@ module.exports = (sequelize, DataTypes) => {
         firstName,
         lastName,
         email,
-        hashedPassword
+        hashedPassword,
       });
 
       return await User.scope('currentUser').findByPk(user.id);
@@ -63,77 +62,94 @@ module.exports = (sequelize, DataTypes) => {
       User.hasOne(models.JobSeeker, { foreignKey: 'userId' });
     }
   }
-  User.init({
-    userTypeId: DataTypes.INTEGER,
-    userName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [4,30],
-        isNotEmail(value) {
-          if (Validator.isEmail(value)) {
-            throw new Error('Cannot be an email.');
-          }
-        }
-      }
-    },
-    firstName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1,30],
-        isNotEmail(value) {
-          if (Validator.isEmail(value)) {
-            throw new Error('Cannot be an email.');
-          }
-        }
-      }
-    },
-    lastName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1,50],
-        isNotEmail(value) {
-          if (Validator.isEmail(value)) {
-            throw new Error('Cannot be an email.');
-          }
-        }
-      }
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [3, 256],
-        isEmail: true
-      }
-    },
-    hashedPassword: {
-      type: DataTypes.STRING.BINARY,
-      allowNull: false,
-      validate: {
-        len: [60, 60]
-      }
-    }
-  }, 
-  {
-    sequelize,
-    modelName: "User",
-    defaultScope: {
-      attributes: {
-        exclude: ["hashedPassword", "email", "createdAt", "updatedAt", "firstName", "lastName", "UserTypeId"]
-      }
-    },
-    scopes: {
-      currentUser: {
-        attributes: { exclude: ["hashedPassword", "email", "firstName", "lastName", "UserTypeId"] }
+  User.init(
+    {
+      userTypeId: DataTypes.INTEGER,
+      userName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [4, 30],
+          isNotEmail(value) {
+            if (Validator.isEmail(value)) {
+              throw new Error('Cannot be an email.');
+            }
+          },
+        },
       },
-      loginUser: {
-        attributes: { exclude: ["UserTypeId"]}
-      }
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1, 30],
+          isNotEmail(value) {
+            if (Validator.isEmail(value)) {
+              throw new Error('Cannot be an email.');
+            }
+          },
+        },
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1, 50],
+          isNotEmail(value) {
+            if (Validator.isEmail(value)) {
+              throw new Error('Cannot be an email.');
+            }
+          },
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [3, 256],
+          isEmail: true,
+        },
+      },
+      hashedPassword: {
+        type: DataTypes.STRING.BINARY,
+        allowNull: false,
+        validate: {
+          len: [60, 60],
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: 'User',
+      defaultScope: {
+        attributes: {
+          exclude: [
+            'hashedPassword',
+            'email',
+            'createdAt',
+            'updatedAt',
+            'firstName',
+            'lastName',
+            'UserTypeId',
+          ],
+        },
+      },
+      scopes: {
+        currentUser: {
+          attributes: {
+            exclude: [
+              'hashedPassword',
+              'email',
+              'firstName',
+              'lastName',
+              'UserTypeId',
+            ],
+          },
+        },
+        loginUser: {
+          attributes: { exclude: ['UserTypeId'] },
+        },
+      },
     }
-  }
   );
   return User;
 };
--- backend-backup/utils/auth.js
+++ backend-backup/utils/auth.js
@@ -5,60 +5,57 @@ const { User } = require('../db/models');
 const { secret, expiresIn } = jwtConfig;
 
 const setTokenCookie = (res, user) => {
-    const token = jwt.sign(
-        { data: user.toSafeObject() },
-        secret,
-        { expiresIn: parseInt(expiresIn) }
-    );
+  const token = jwt.sign({ data: user.toSafeObject() }, secret, {
+    expiresIn: parseInt(expiresIn),
+  });
 
-    const isProduction = process.env.NODE_ENV === 'production';
+  const isProduction = process.env.NODE_ENV === 'production';
 
-    res.cookie('token', token, {
-        maxAge: expiresIn * 1000,
-        httpOnly: true,
-        secure: isProduction,
-        sameSite: isProduction && 'Lax'
-    });
+  res.cookie('token', token, {
+    maxAge: expiresIn * 1000,
+    httpOnly: true,
+    secure: isProduction,
+    sameSite: isProduction && 'Lax',
+  });
 
-    return token;
+  return token;
 };
 
 const restoreUser = (req, res, next) => {
-    // token parsed from cookies
-    const { token } = req.cookies;
-    
-    req.user = null;
-  
-    return jwt.verify(token, secret, null, async (err, jwtPayload) => {
-      if (err) {
-        return next();
-      }
-  
-      try {
-        const { id } = jwtPayload.data;
-        req.user = await User.scope('currentUser').findByPk(id);
-        console.log(req.user)
-    } catch (e) {
-        res.clearCookie('token');
-        return next();
-      }
-  
-      if (!req.user) res.clearCookie('token');
-  
+  // token parsed from cookies
+  const { token } = req.cookies;
+
+  req.user = null;
+
+  return jwt.verify(token, secret, null, async (err, jwtPayload) => {
+    if (err) {
       return next();
-    });
+    }
+
+    try {
+      const { id } = jwtPayload.data;
+      req.user = await User.scope('currentUser').findByPk(id);
+    } catch (e) {
+      res.clearCookie('token');
+      return next();
+    }
+
+    if (!req.user) res.clearCookie('token');
+
+    return next();
+  });
 };
 
 const requireAuth = function (req, _res, next) {
-    if (req.user) return next();
-  
-    const err = new Error('Authentication required');
+  if (req.user) return next();
 
-    err.title = 'Authentication required';
-    err.errors = { message: 'Authentication required' };
-    err.status = 401;
+  const err = new Error('Authentication required');
 
-    return next(err);
+  err.title = 'Authentication required';
+  err.errors = { message: 'Authentication required' };
+  err.status = 401;
+
+  return next(err);
 };
 
 module.exports = { setTokenCookie, restoreUser, requireAuth };
